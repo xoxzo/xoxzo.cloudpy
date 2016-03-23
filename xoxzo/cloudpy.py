a1fdc73d97d0c578942aa6a884cd6d3a2b7fa1ec
Akira Nonaka <nonaka@mac.com>
2016-03-23 14:34:38 +0900

    added get_delivery_status. needs refactor.

--- xoxzo/cloudpy.py
+++ xoxzo/cloudpy.py
@@ -3,7 +3,7 @@
 import urllib
 import urllib2
 
-def send_sms_message(sid, auth_token,message,recipient,sender):
+def send_sms_message(sid,auth_token,message,recipient,sender):
     url = "https://api.xoxzo.com/sms/messages/"
     user_agent = "xoxzo cloudpy 1.0"
     values = {'message' : message,
@@ -14,6 +14,34 @@ def send_sms_message(sid, auth_token,message,recipient,sender):
 
     data = urllib.urlencode(values)
     req = urllib2.Request(url, data, headers)
+
+    # create a password manager
+    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+
+    # Add the username and password.
+    # If we knew the realm, we could use it instead of ``None``.
+    password_mgr.add_password(None, url, sid, auth_token)
+
+    handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+
+    # create "opener" (OpenerDirector instance)
+    opener = urllib2.build_opener(handler)
+    urllib2.install_opener(opener)
+
+    response = urllib2.urlopen(req)
+    return response.read()
+
+def get_delivery_status(sid,auth_token,msgid):
+    url = "https://api.xoxzo.com/sms/messages/" + msgid
+    user_agent = "xoxzo cloudpy 1.0"
+    # values = {'message' : message,
+    #           'recipient' : recipient,
+    #           'sender' : sender }
+
+    headers = { 'User-Agent' : user_agent }
+
+    #data = urllib.urlencode(values)
+    req = urllib2.Request(url, None, headers)
 
     # create a password manager
     password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
