5422af31083ee629f7e48a53beb554df418ee1db
Akira Nonaka <nonaka@mac.com>
2016-03-24 08:47:41 +0900

    get status updated

--- xoxzo/cloudpy/cloudpy.py
+++ xoxzo/cloudpy/cloudpy.py
@@ -3,18 +3,26 @@
 import requests
 import json
 
+
 class XoxzoCloud:
-    def __init__(self,sid,auth_token):
+    def __init__(self, sid, auth_token):
         self.sid = sid
         self.auth_token = auth_token
 
-    def send_sms(self,message,recipient,sender):
+    def send_sms(self, message, recipient, sender):
         url = "https://api.xoxzo.com/sms/messages/"
-        payload = {'message' : message,
-              'recipient' : recipient,
-              'sender' : sender }
-        r = requests.post(url, data=payload,auth=(self.sid,self.auth_token))
-        return r
+        payload = {
+            'message': message,
+            'recipient': recipient,
+            'sender': sender}
+        self.send_sms_last_respose = requests.post(
+            url,
+            data=payload,
+            auth=(self.sid, self.auth_token))
+        # remember the last response for future use
+        return self.send_sms_last_respose
 
-    def get_delivery_status(self,msgid):
+    def get_delivery_status(self, msgid):
         url = "https://api.xoxzo.com/sms/messages/" + msgid
+        r = requests.post(url, auth=(self.sid, self.auth_token))
+        return r
