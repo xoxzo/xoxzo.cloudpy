058674b064ba76047d7348ebdfb2f8deb4f865c7
Akira Nonaka <nonaka@mac.com>
2016-03-24 10:46:02 +0900

    added voice api

--- xoxzo/cloudpy/cloudpy.py
+++ xoxzo/cloudpy/cloudpy.py
@@ -9,6 +9,7 @@ class XoxzoCloud:
         self.sid = sid
         self.auth_token = auth_token
         self.send_sms_last_respose = None
+        self.voice_palyback_last_respose = None
 
     def send_sms(self, message, recipient, sender):
         url = "https://api.xoxzo.com/sms/messages/"
@@ -23,7 +24,25 @@ class XoxzoCloud:
         # remember the last response for future use
         return self.send_sms_last_respose
 
-    def get_delivery_status(self, msgid):
+    def get_sms_delivery_status(self, msgid):
         url = "https://api.xoxzo.com/sms/messages/" + msgid
+        r = requests.post(url, auth=(self.sid, self.auth_token))
+        return r
+
+    def voice_playback(self, caller, recipient, recording_url):
+        url = "https://api.xoxzo.com/voice/simple/playback/"
+        payload = {
+            'caller': caller,
+            'recipient': recipient,
+            'recording_url': recording_url}
+        self.voice_palyback_last_respose = requests.post(
+            url,
+            data=payload,
+            auth=(self.sid, self.auth_token))
+        # remember the last response for future use
+        return self.voice_palyback_last_respose
+
+    def get_voice_playback_status(self, callid):
+        url = "https://api.xoxzo.com/voice/simple/playback/" + callid
         r = requests.post(url, auth=(self.sid, self.auth_token))
         return r
