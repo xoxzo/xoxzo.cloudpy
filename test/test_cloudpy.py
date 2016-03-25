a426c9cd1f204cf6d3d215073a350aabc09a3d02
Akira Nonaka <nonaka@mac.com>
2016-03-25 16:35:55 +0900

    disable some test cases

--- test/test_cloudpy.py
+++ test/test_cloudpy.py
@@ -2,6 +2,7 @@
 import unittest
 import json
 import os
+import requests
 
 from xoxzo.cloudpy import XoxzoClient
 
@@ -10,20 +11,60 @@ class TestXoxzoClient(unittest.TestCase):
     def setUp(self):
         self.test_recipient = os.environ.get("XOXZO_API_TEST_RECIPIENT")
         self.test_sender = "04512345678"
+        self.test_mp3_url = os.environ.get("XOXZO_API_TEST_MP3")
+
+        print
         print "Test recipient: %s" % self.test_recipient
         print "Test sender: %s" % self.test_sender
+        print "Test MP3 url: %s" % self.test_mp3_url
 
-    def test_send_sms_success(self):
+    def test_send_sms_success01(self):
+        return # skip for now
         xc = XoxzoClient()
         response = xc.send_sms(
             "Hello from Xoxzo",
             self.test_recipient,
             self.test_sender)
         self.dump_response(response)
-        # get msgid from response
-        # msgid = response.json()[0]['msgid']
+        print response.status_code
+        self.assertEqual(201, response.status_code)
+
         response = xc.get_sms_delivery_status()
         self.dump_response(response)
+        print response.status_code
+        self.assertEqual(requests.codes.ok, response.status_code)
+
+    def test_voice_simple_playback_success01(self):
+        return # skip for now
+        xc = XoxzoClient()
+        response = xc.voice_simple_playback(
+            self.test_sender,
+            self.test_recipient,
+            self.test_mp3_url)
+
+        self.dump_response(response)
+        print response.status_code
+        self.assertEqual(201, response.status_code)
+
+        response = xc.get_voice_simple_playback_status()
+        self.dump_response(response)
+        print response.status_code
+        self.assertEqual(requests.codes.ok, response.status_code)
+
+    def test_bad_sid(self):
+        xc = XoxzoClient("123", "456")
+        response = xc.send_sms("123", "123", "123")
+        self.assertEqual(401,response.status_code)
+
+    def test_get_sms_delivery_status_fail01(self):
+        # bad msgid
+        xc = XoxzoClient()
+        response = xc.get_sms_delivery_status("123456")
+        #self.dump_response(response)
+        self.assertEqual(404,response.status_code)
 
     def dump_response(self, response):
         print json.dumps(response.json(), indent=4)
+
+    if __name__ == "__main__":
+        unittest.main()
