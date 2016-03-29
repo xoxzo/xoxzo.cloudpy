8844b26fb81bee409ffccc1a32d3fe6d4b7ddedf
Akira Nonaka <nonaka@mac.com>
2016-03-29 11:18:17 +0900

    WIP, added sample code. minor fix for tests.

--- README.rst
+++ README.rst
@@ -5,7 +5,7 @@ Xoxzo Cloud API Client for Python
 This is the official client and implementation reference to talk to `Xoxzo's Cloud Communication API <https://www.xoxzo.com/en/>`_.
 For detailed documentation on the API itself, you can refer to the `documentation <http://docs.xoxzo.com/en/>`_
 
-** sample code
+**sample code**
 
 send sms::
 
@@ -19,4 +19,17 @@ send sms::
       recipient = "+818012345678",
       sender = "818011112222")
   result = xc.get_sms_delivery_status()
+  print json.dumps(result.json(), indent=4)
+
+
+play back mp3 file::
+
+  # sample 02 call voice playback
+
+  xc = XoxzoClient(sid="<your xoxzo sid>", auth_token="<your xoxzo auth_token>")
+  xc.call_simple_playback(
+      recipient = "+818012345678",
+      recording_url = "http://example.com/sample.mp3",
+      caller = "818011112222")
+  result = xc.get_simple_playback_status()
   print json.dumps(result.json(), indent=4)
--- tests/test_cloudpy.py
+++ tests/test_cloudpy.py
@@ -13,29 +13,27 @@ class TestXoxzoClient(unittest.TestCase):
         self.test_sender = "04512345678"
         self.test_mp3_url = os.environ.get("XOXZO_API_TEST_MP3")
 
-        print
-        print "Test recipient: %s" % self.test_recipient
-        print "Test sender: %s" % self.test_sender
-        print "Test MP3 url: %s" % self.test_mp3_url
+        # print
+        # print "Test recipient: %s" % self.test_recipient
+        # print "Test sender: %s" % self.test_sender
+        # print "Test MP3 url: %s" % self.test_mp3_url
 
     def test_send_sms_success01(self):
-        return  # skip for now
+        # return  # skip for now
         xc = XoxzoClient()
         response = xc.send_sms(
             "Hello from Xoxzo",
             self.test_recipient,
             self.test_sender)
         self.dump_response(response)
-        print response.status_code
         self.assertEqual(201, response.status_code)
 
         response = xc.get_sms_delivery_status()
         self.dump_response(response)
-        print response.status_code
         self.assertEqual(requests.codes.ok, response.status_code)
 
     def test_call_simple_playback_success01(self):
-        return  # skip for now
+        # return  # skip for now
         xc = XoxzoClient()
         response = xc.call_simple_playback(
             self.test_sender,
@@ -43,12 +41,10 @@ class TestXoxzoClient(unittest.TestCase):
             self.test_mp3_url)
 
         self.dump_response(response)
-        print response.status_code
         self.assertEqual(201, response.status_code)
 
         response = xc.get_simple_playback_status()
         self.dump_response(response)
-        print response.status_code
         self.assertEqual(requests.codes.ok, response.status_code)
 
     def test_bad_sid(self):
@@ -65,7 +61,6 @@ class TestXoxzoClient(unittest.TestCase):
             self.test_sender)
 
         self.dump_response(response)
-        print response.status_code
         self.assertEqual(400, response.status_code)
 
     def test_send_sms_fail02(self):
@@ -107,7 +102,6 @@ class TestXoxzoClient(unittest.TestCase):
             self.test_mp3_url)
 
         self.dump_response(response)
-        print response.status_code
         self.assertEqual(400, response.status_code)
 
     def test_call_simple_playback_fail02(self):
@@ -142,7 +136,14 @@ class TestXoxzoClient(unittest.TestCase):
         self.assertEqual(404, response.status_code)
 
     def dump_response(self, response):
-        print json.dumps(response.json(), indent=4)
+        print
+        print "---------------------"
+        print response.status_code
+        print response.text
+        print
+        j = response.json()
+        print type(j)
+        print json.dumps(j, indent=4)
 
     if __name__ == "__main__":
         unittest.main()
