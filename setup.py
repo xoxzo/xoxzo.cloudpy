ce9eb07224e9f2f6c46af5b093117a0fae643c30
Hyejeong Park <iam@hyejeong-park.com>
2017-12-17 14:51:22 +0900

    Add namespace pakages

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="xoxzo.cloudpy",
-    version="0.3.2",
+    name="xoxzo_cloudpy",
+    version="0.4.0",
     author="Xoxzo Inc.",
     author_email="help@xoxzo.com",
     description=("Xoxzo Public API library"),
     license="MIT",
     keywords="xoxzo telephony api",
     url="https://github.com/xoxzo/xoxzo.cloudpy",
-    packages=find_packages(),
+    packages=find_packages('.'),
+    namespace_packages=['xoxzo'],
     install_requires=[
         'requests >=2.9.1',
     ],
