677ee33f78adbd379c6f22fc0426ae03f7b0ba21
Hyejeong Park <phj1698@gmail.com>
2018-01-08 13:36:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xoxzo.cloudpy",
-    version="0.4.0",
+    version="0.4.1",
     author="Xoxzo Inc.",
     author_email="help@xoxzo.com",
     description=("Xoxzo Public API library"),
